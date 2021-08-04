e04f671a2e867b38e34f81554eba26815a4489f6
ekvictorson <vier0801@gmail.com>
2021-08-04 16:50:07 -0700

    update for ubuntu 20.04

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -62,66 +62,34 @@ sudo apt install atom
 sudo apt install curl
 
 # install pip
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py
+#curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+#python get-pip.py
+sudo apt install python3-pip
 
 # install numpy and others (system wide)
-sudo apt install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
+sudo apt install python3-numpy python3-scipy python3-matplotlib python3-pandas python3-sympy python3-nose
+
+pip install ipython
+pip install ipython-notebook
 
 # install scikit-learn
 pip install -U scikit-learn
 
 # install opencv
-sudo apt install libjpeg-dev libpng-dev libtiff-dev
-sudo apt install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-sudo apt install libxvidcore-dev libx264-dev
-sudo apt install libgtk-3-dev
-sudo apt install libatlas-base-dev gfortran
-sudo apt install python3-dev
-mkdir ~/libraries
-cd ~/libraries
-wget -O opencv.zip https://github.com/opencv/opencv/archive/3.4.4.zip
-wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/3.4.4.zip
-unzip opencv.zip
-unzip opencv_contrib.zip
-cd ~/libraries/opencv-3.4.4
-mkdir build
-cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE \
-	-D CMAKE_INSTALL_PREFIX=/usr/local \
-	-D INSTALL_PYTHON_EXAMPLES=ON \
-	-D INSTALL_C_EXAMPLES=OFF \
-	-D OPENCV_ENABLE_NONFREE=ON \
-	-D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
-	-D PYTHON_EXECUTABLE=~/.virtualenvs/cv/bin/python \
-	-D BUILD_EXAMPLES=ON ..
-make -j4
-sudo make install
-sudo ldconfig
-pkg-config --modversion opencv
-cd ~
+sudo apt install libopencv-dev python3-opencv
 
-# install ROS kinetic
-#sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-#sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-#sudo apt update
-#sudo apt install ros-melodic-desktop-full
-#sudo rosdep init
-#rosdep update
-#echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
-#source ~/.bashrc
-#sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
-
+# install ros noetic
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-sudo apt-get update
-sudo apt-get install ros-kinetic-desktop-full
+sudo apt install curl # if you haven't already installed curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt update
+sudo apt install ros-noetic-desktop-full
+source /opt/ros/noetic/setup.bash
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essentia
 sudo rosdep init
 rosdep update
-echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
-source ~/.bashrc
-sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
-
 
 # overlay fortune in bashrc
 echo "fortune" >> ~/.bashrc
